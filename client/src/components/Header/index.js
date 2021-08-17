8ba0f26c003f64b37118098b2478087668b55b41
Kyle Berta <80055751+IBnIT@users.noreply.github.com>
2021-08-17 05:59:43 -0700

    add more css styling update title add folder for background image

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom'
 const Header =()=> {
     return (
         <header className="is-flex is-justify-content-space-around is-align-items-center is-flex-wrap-wrap">
-            <h1 className="is-size-2"><Link to="/" style={{color: "rgb(4, 185, 50)"}}>Mental Health</Link></h1>
+            <h1 className="is-size-2"><Link to="/" style={{color: "rgb(4, 185, 50)"}}>Plan Community</Link></h1>
             <nav>
                 <ul>
-                    <Link to="/signin">Sign In</Link>
-                    <Link to="/signup">Sign Up</Link>
+                    <li>
+                    <Link to="/signin" style={{color: "rgb(4, 185, 50)"}} >Sign In</Link> 
+                    </li>
+                    <li>
+                    <Link to="/signup" style={{color: "rgb(4, 185, 50)"}} >Sign Up</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
